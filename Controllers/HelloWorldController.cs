05e5a4070b8c9032e10600af2f231f14e43f2f01
Bruno Hora <bruno.hora@gpssa.com.br>
2024-09-05 22:18:47 -0300

    Inclusão de api e arquivo yml

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fiap_githubactions_03.Controllers
+{
+    [Route("api/hello-world")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok("Hello World!");
+        }
+    }
+}
